370a71bbfcf273e12b295f1c7d816b67807ad18b
mtrietqi <mtrietqi@gmail.com>
2019-04-12 19:48:57 +0700

    initialize server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("hello")
+/* 3rd packages */
+const express = require('express');
+/* project packages */
+
+/* initialize server */
+const app= express();
+
+const port = process.env.PORT || 5000
+app.listen(port,()=>{
+    console.log('Connected to DB on ' + port)
+})
